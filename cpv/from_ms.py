79786847ad31014e65c5ddf2222c8add715cf656
Philippe Aucazou <paucazou@yahoo.fr>
2019-05-17 03:41:39 +0200

    added wrapper for MuseScore xml files

---
+++ cpv/from_ms.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*-coding:Utf-8 -*
+#Deus, in adjutorium meum intende
+
+import __cpv
+import importer
+
+__current_module = __import__(__name__)
+
+for fun_name in __cpv.functions:
+    fun = getattr(__cpv,fun_name)
+    def decorator(func):
+        def wrapper(file, *a, **kw):
+            content = importer.import_msx(file)
+            return func(content,*a,**kw)
+        return wrapper
+
+    setattr(__current_module,fun_name,decorator(fun))
