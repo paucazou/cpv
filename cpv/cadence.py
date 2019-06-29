dee541250209d33a7c0286444ad55574c2c5b744
Philippe Aucazou <paucazou@yahoo.fr>
2019-06-29 07:05:31 +0200

    Added examples

--- cpv/cadence.py
+++ cpv/cadence.py
@@ -3,3 +3,19 @@
 #Deus, in adjutorium meum intende
 """Represents candences"""
 
+import chord
+import enum
+
+class Cadence:
+    class Type(enum.Enum):
+        PerfectAuthentic = enum.auto()
+        ImperfectAuthenc = enum.auto()
+        Evaded = enum.auto()
+        Half = enum.auto()
+        PhrygianHalf = enum.auto()
+        Lydian = enum.auto()
+        Burgundian = enum.auto()
+        PlagalHalf = enum.auto()
+        Plagal = enum.auto()
+        Deceptive = enum.auto()
+
